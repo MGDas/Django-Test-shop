ca026a1362a2cb14a7cdf75f695a5a45c42d2a59
MGDas <mgdas051@gmail.com>
2019-03-07 16:43:27 +0300

    Admin customization

--- orders/admin.py
+++ orders/admin.py
@@ -1,16 +1,55 @@
 from django.contrib import admin
 from .models import Order, OrderItem
+from django.http import HttpResponse
+from django.shortcuts import reverse
+from django.utils.html import format_html
+import csv
+import datetime
 
 
-class OrderItemInline(admin.TabularInline):
-    model = OrderItem
-    raw_id_field = ['product']
+
+def ExportToCSV(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; \
+            filename=Orders-{}.csv'.format(datetime.datetime.now().strftime('%d/%m/%Y'))
+    writer = csv.writer(response)
+
+    fields = [field for field in opts.get_field() if not field.many_to_many and field.one_to_many]
+    # Первая строка - оглавление
+    writer.writerow([field.verbose_name for field in fields])
+    # Заполняем информацией
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+    ExportToCSV.short_description = 'Export CSV'
+
+
+def OrderDetail(obj):
+    return format_html('<a href="{}">Посмотреть</a>'.format(
+        reverse('admin_order_detail_url', args=[obj.id])
+    ))
+
+
+# class OrderItemInline(admin.TabularInline):
+#     model = OrderItem
+#     raw_id_field = ['product']
 
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'email', 'address',
-                    'postal_code', 'city', 'paid', 'created', 'updated']
+    list_display = ['last_name', 'first_name', 'email', 'address',
+                    'postal_code', 'city', 'paid', 'created', 'updated', OrderDetail]
+    readonly_fields = ['id', 'first_name', 'last_name', 'email', 'address',
+                    'postal_code', 'city', 'paid', 'created', 'updated', OrderDetail]
     list_filter = ['paid', 'created', 'updated']
-    inlines = [OrderItemInline]
+    # inlines = [OrderItemInline]
+    actions = [ExportToCSV]
+
 
 admin.site.register(Order, OrderAdmin)
--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
-from .views import OrderCreate
+from django.urls import path, re_path
+from .views import OrderCreate, AdminOrderDetail
 
 
 urlpatterns = [
-    path('create/', OrderCreate, name='order_create_url')
+    path('create/', OrderCreate, name='order_create_url'),
+    re_path(r'^admin/order/(?P<order_id>\d+)/$', AdminOrderDetail, name='admin_order_detail_url'),
 ]
